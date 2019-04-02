f67ac9e9fbddba8d71cbece3937eaa4f7e42dbf2
Sinclair <sinfram248@gmail.com>
2019-04-02 12:03:10 +1100

    Added Mechanics

---
+++ Assets/Scripts/Pieces/Boomerang.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boomerang : MonoBehaviour {
+
+	public float damage;
+	public List<GameObject> enemies = new List<GameObject>();
+
+	void OnTriggerEnter(Collider other) {
+		GameObject otherGameObject = other.gameObject;
+		if (otherGameObject.CompareTag("Enemy")) {
+			if (!enemies.Contains(otherGameObject)) {
+				enemies.Add(otherGameObject);
+				otherGameObject.GetComponent<Enemy>().health -= damage;
+			}
+		}
+	}
+}
