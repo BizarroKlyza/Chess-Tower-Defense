54d62ba5d3bf563acf2719b740bd022a56ec8daa
Sinclair <sinfram248@gmail.com>
2019-04-03 12:06:05 +1100

    Added enemies/scripts/explosions

---
+++ Assets/Scripts/Enemies/EnemyTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour {
+
+	List<GameObject> targets = new List<GameObject>();
+	Enemy par;
+
+	void Start() {
+		par = transform.parent.parent.gameObject.GetComponent<Enemy>();
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			par.attacking = true;
+			targets.Add(other.gameObject);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			if (targets.Count == 0) {
+				par.attacking = false;
+			}
+			targets.Remove(other.gameObject);
+		}
+		
+	}
+}
