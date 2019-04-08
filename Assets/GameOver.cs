57eb7c745542dee7c801c3eceb537e57f404ff9c
Sinclair <sinfram248@gmail.com>
2019-04-08 11:14:11 +1000

    Added win/lose conditions

---
+++ Assets/GameOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+	public GameObject lostMenu;
+	public GameObject wonMenu;
+
+	public void Lost() {
+		Time.timeScale = 0.2f;
+		lostMenu.SetActive(true);
+	}
+	
+	public void Won() {
+		Time.timeScale = 0.2f;
+		wonMenu.SetActive(true);
+	}
+
+	public void ExitToMain() {
+		Time.timeScale = 1.0f;
+		lostMenu.SetActive(false);
+		wonMenu.SetActive(false);
+		SceneManager.LoadScene(0);
+	}
+}
---
+++ Assets/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+	public GameOver gameOver;
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Enemy") {
+			gameOver.Lost();
+		}
+	}
+}
